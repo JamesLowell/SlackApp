04bea8d1c4c616c1d90719d5f3ce57998dc4e1e6
James Lowell Mamac <mjameslowell26@gmail.com>
2023-11-16 11:26:33 +0800

    adding sidebar route

--- not-slack-app/src/main.jsx
+++ not-slack-app/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import SideBar from './SideBar'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+
+const router = createBrowserRouter([
+  {path:'messages',
+  element:<SideBar/>}
+
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SideBar />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
