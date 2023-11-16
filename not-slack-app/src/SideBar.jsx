66fa647b4fa42a66263f95f6e0e7207bd1a1531f
James Lowell Mamac <mjameslowell26@gmail.com>
2023-11-16 11:16:29 +0800

    installing react vite and adding navbar

---
+++ not-slack-app/src/SideBar.jsx
@@ -0,0 +1,68 @@
+import './SideBar.css'
+import { CiSearch } from "react-icons/ci";
+
+function SideBar() {
+
+  return (
+    <>
+      <div className='side-bar'>
+        <span>Chats</span>
+        <input type="text" placeholder='Search'/>
+        <icon><CiSearch /></icon>
+        <hr />
+        <ul className='user-list'>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+          <li>James Lowell Mamac</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default SideBar
---
+++ not-slack-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SideBar from './SideBar'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SideBar />
+  </React.StrictMode>,
+)
