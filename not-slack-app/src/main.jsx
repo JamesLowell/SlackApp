d627ea6569abf3d76f61e7fb020097c32a06b79d
James Lowell Mamac <mjameslowell26@gmail.com>
2023-11-16 17:40:05 +0800

    creating landing page

--- not-slack-app/src/main.jsx
+++ not-slack-app/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import SideBar from './SideBar'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
+import LandingPage from './LandingPage'
 
 const router = createBrowserRouter([
+  {index:'true',
+  element:<LandingPage/>},
   {path:'messages',
   element:<SideBar/>}
-
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
